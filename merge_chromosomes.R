ec0c8c750e142584aea600ef28512fdee0678e1d
Benno Pütz <puetz@psych.mpg.de>
2022-12-09 01:02:21 +0100

    mostly roxygen comments (started)

--- merge_chromosomes.R
+++ merge_chromosomes.R
@@ -4,38 +4,74 @@ library(parallel)
 
 path = '../Chromosom1_22_rda'
 
+#' Read one Roselli chromosome data file 
+#'
+#' 
+#' @param file 
+#' @param path 
+#' @param drop.NA 
+#' @param which.cols 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 read.one <- function(file,
                      path       = '.',
-		     drop.NA    = TRUE,
-		     which.cols = c(1,4),
+                     drop.NA    = TRUE,
+                     which.cols = c(1,4),
                      ...){
   var <- load(file.path(path,file),     # var is name of variable just loaded
 	      ...)  
   one <- get(var)
   if(drop.NA && ncol(one)==4) {
     one <- subset(one,
-	 	  subset = !is.na(one[,2]),  # non-NA rows
-	          select = which.cols)       # rs + chr:pos
+                  subset = !is.na(one[,2]),  # non-NA rows
+                  select = which.cols)       # rs + chr:pos
   } else {
     one <- one[,which.cols]
   }
   assign(var,
          one,
-         env = .GlobalEnv)   # make sure the variable is created in the global
-                             # environment to be availble for the seesion
+         envir = .GlobalEnv)   # make sure the variable is created in the global
+                               # environment to be availble for the seesion
 }
 
+#' Read all Roselli chromosome data files
+#'
+#' @param base 
+#' @param ext 
+#' @param path 
+#' @param index 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 read.all <- function(base  = 'new_chr',
-	    	     ext   = '.rda', 
-		     path  = '.',
-		     index = 1:22,
-		     ...){
-
+                     ext   = '.rda', 
+                     path  = '.',
+                     index = 1:22,
+                     ...){
+  
   lapply(index, function(i){read.one(file = paste0(base,i,ext),
-				     path = path,
-				     ...)})
+                                     path = path,
+                                     ...)})
 }
 
+#' combine previously loaded Roselli chromosome dataframes
+#'
+#'
+#' @param base base name of input variables
+#' @param index vector if chromosome indices to process
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 combine.all <- function(base  = 'new_chr',
                         index = 1:22,
                         ...){
@@ -47,9 +83,28 @@ combine.all <- function(base  = 'new_chr',
   return(all)
 }
 
-do.all <- function(...){
+#' read and combine  Roselli chromosome data files
+#'
+#' The merged dataframe is saved in RDS format and also returned (invisibly)
+#'
+#' @param outfile name of outputfile (extension ".rds" optional)
+#' @param ... passed on to all internal calls
+#'
+#' @return merged dataframe (invisibly)
+#' @export
+#'
+#' @examples
+do.all <- function(outfile = "merge.tbl",
+                   ...){
     read.all(path = path,
              ...)
     merge.tbl <- combine.all(...)
-    saveRDS(merge.tbl, file="merge.tbl.rds")
+    if (!grepl("\\.rds$", 
+               outfile, 
+               ignore.case = TRUE)){
+      outfile <- paste0(outfile, ".rds")
+    }
+    saveRDS(merge.tbl,
+            file = outfile)
+    return(invisible(merge.tbl))
 }
