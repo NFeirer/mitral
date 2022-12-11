3b9b7e5a61eaeac1aaa937c9b41df8d32c8b81d8
NFeirer <120017242+NFeirer@users.noreply.github.com>
2022-12-11 17:34:55 +0100

    Create merge_all_chr_Roselli.R

---
+++ merge_all_chr_Roselli.R
@@ -0,0 +1,73 @@
+#aim: make a list of all 22 rds-files which contain the data of Roselli and the reference chromosomes combined
+#example for a name of the files: "chr1_roselli.rds"
+
+library(dplyr)
+library(data.table)
+library(parallel)
+
+path = #is currently home directory, might move all 22 rds-files into a directory for better organisation
+
+#read one chr_roselli file (including all columns)
+
+read.one <- function(file,
+                     path       = '.',
+                     drop.NA    = TRUE,
+                     #which.cols = c(1,4), -> not needed as we want all columns now
+                     ...){
+  var <- load(file.path(path,file),     # var is name of variable just loaded
+              ...)  
+  one <- get(var)
+  # if(drop.NA && ncol(one)==4) {
+  #   one <- subset(one,
+  #                 subset = !is.na(one[,2]),  # non-NA rows
+  #                 select = which.cols)       # rs + chr:pos
+  # } else {
+  #   one <- one[,which.cols]
+  # }
+  assign(var,
+         one,
+         envir = .GlobalEnv)   # make sure the variable is created in the global
+  # environment to be availble for the seesion
+}
+
+#read all chr_roselli files
+read.all <- function(base  = 'chr', 
+                     ext   = '_roselli.rds', #is this correct?
+                     path  = '.',
+                     index = 1:22,
+                     ...){
+  
+  lapply(index, function(i){read.one(file = paste0(base,i,ext),
+                                     path = path,
+                                     ...)})
+}
+
+#' combine previously loaded chromosome_roselli dataframes
+
+combine.all <- function(base  = 'new_chr',
+                        index = 1:22,
+                        ...){
+  all <- eval(parse(text = paste0('rbind(',
+                                  paste0(base,
+                                         index,
+                                         collapse = ","),
+                                  ")")))
+  return(all)
+}
+
+#' read and combine chromosome_roselli data files
+
+do.all <- function(outfile = "all_refchr_roselli",
+                   ...){
+  read.all(path = path,
+           ...)
+  all_refchr_roselli <- combine.all(...)
+  if (!grepl("\\.rds$", 
+             outfile, 
+             ignore.case = TRUE)){
+    outfile <- paste0(outfile, ".rds")
+  }
+  saveRDS(all_refchr_roselli,
+          file = outfile)
+  return(invisible(all_refchr_roselli))
+}
